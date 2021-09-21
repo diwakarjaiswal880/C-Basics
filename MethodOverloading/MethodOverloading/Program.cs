4a71b4dca9c508d91889c72283fb7e70f546e287
Diwakar Jaiswal <38224086+diwakarjaiswal880@users.noreply.github.com>
2021-09-21 11:23:54 +0530

    Add files via upload

---
+++ MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MethodOverloading
+{
+    public class Diwakar
+    {
+        public static int add(int a, int b)
+        {
+            return a + b;
+        }
+        public static float add(float a, float b)
+        {
+            return a + b;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Diwakar.add(12, 23));
+            Console.WriteLine(Diwakar.add(12.4f, 21.3f));
+        }
+    }
+}
---
+++ MultiLevelInheritance/MultiLevelInheritance/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MultiLevelInheritance
+{
+    class Shape
+    {
+        public void setWidth(int w)
+        {
+            width = w;
+        }
+        public void setHeight(int h)
+        {
+            height = h;
+        }
+        protected int width;
+        protected int height;
+    }
+    public interface PaintCost
+    {
+        int getCost(int area);
+    }
+    class Rectangle : Shape, PaintCost
+    {
+        public int getArea()
+        {
+            return (width * height);
+        }
+        public int getCost(int area)
+        {
+            return area * 70;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle Rect = new Rectangle();
+            int area;
+            Rect.setWidth(5);
+            Rect.setHeight(7);
+            area = Rect.getArea();
+            Console.WriteLine("Total area: {0}", Rect.getArea());
+            Console.WriteLine("Total paint cost: ${0}", Rect.getCost(area));
+            Console.ReadKey();
+        }
+    }
+}
