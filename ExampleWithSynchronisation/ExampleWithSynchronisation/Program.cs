aa1b4ac35bbbd0d74142ae3b23d18ec8aefe66fb
Diwakar Jaiswal <38224086+diwakarjaiswal880@users.noreply.github.com>
2021-09-21 11:06:00 +0530

    Add files via upload

---
+++ ExampleWithSynchronisation/ExampleWithSynchronisation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+class Diwakar
+{
+    public void PrintTable()
+    {
+        lock (this)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Thread.Sleep(100);
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
+namespace ExampleWithSynchronisation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diwakar p = new Diwakar();
+            Thread t1 = new Thread(new ThreadStart(p.PrintTable));
+            Thread t2 = new Thread(new ThreadStart(p.PrintTable));
+            t1.Start();
+            t2.Start();
+        }
+    }
+}
---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int a = 10;
+                int b = 0;
+                int x = a / b;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.WriteLine("This message is from catch block");
+        }
+    }
+}
