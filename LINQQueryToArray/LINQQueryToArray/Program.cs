669aa51e949b75a8132ea226254a1bae90869add
Diwakar Jaiswal <38224086+diwakarjaiswal880@users.noreply.github.com>
2021-09-21 11:17:07 +0530

    Add files via upload

---
+++ LINQQueryToArray/LINQQueryToArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQQueryToArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            var lowNums = (from n in numbers
+
+                           where (n % 2 == 0)
+                          
+                           orderby n
+
+                           select n);
+
+
+            Console.WriteLine("Select even number only");
+            foreach(var i in lowNums)
+            {
+                Console.Write(i+ " ");
+            }
+            Console.WriteLine();
+        
+        }
+    }
+}
---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new LinkedList<string>();
+            names.AddLast("Rajat");
+            names.AddLast("Arun");
+            names.AddLast("Prakash");
+            names.AddLast("jay");
+            names.AddFirst("sai");
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
