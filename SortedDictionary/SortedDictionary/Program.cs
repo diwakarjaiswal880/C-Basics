d83746b916992d3398cf251f5952631632d480d2
Diwakar Jaiswal <38224086+diwakarjaiswal880@users.noreply.github.com>
2021-09-21 11:54:37 +0530

    Add files via upload

---
+++ SortedDictionary/SortedDictionary/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortedDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, string> names = new SortedDictionary<string, string>();
+            names.Add("1", "Arun");
+            names.Add("4", "Vishal");
+            names.Add("5", "Ramesh");
+            names.Add("7", "Vidya");
+            names.Add("2", "Pallavi");
+            foreach (KeyValuePair<string, string> kv in names)
+            {
+                Console.WriteLine(kv.Key + " " + kv.Value);
+            }
+        }
+    }
+}
---
+++ SortedList/SortedList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<int,string> names = new SortedList<int, string>();
+            names.Add(1, "Arun");
+            names.Add(4, "Vishal");
+            names.Add(5, "Ramesh");
+            names.Add(3, "Vidya");
+            names.Add(2, "Pallavi");
+            foreach (KeyValuePair<int, string> kv in names)
+            {
+                Console.WriteLine(kv.Key + " " + kv.Value);
+            }
+
+            SortedList<string, string> cities = new SortedList<string, string>()
+                                    {
+                                        {"London", "UK"},
+                                        {"New York", "USA"},
+                                        { "Mumbai", "India"},
+                                        {"Johannesburg", "South Africa"}
+                                    };
+            foreach (KeyValuePair<string, string> kv in cities)
+            {
+                Console.WriteLine(kv.Key + " " + kv.Value);
+            }
+        }
+    }
+}
