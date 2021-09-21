1bc0c0c7ea240b2b39896566e8089216645b0af3
Diwakar Jaiswal <38224086+diwakarjaiswal880@users.noreply.github.com>
2021-09-21 10:05:39 +0530

    Add files via upload

---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        private double length;   // Length of a line
+
+        public Program()
+        {
+            Console.WriteLine("Object is being created");
+        }
+        public void setLength(double len)
+        {
+            length = len;
+        }
+        public double getLength()
+        {
+            return length;
+        }
+
+        static void Main(string[] args)
+        {
+            Program line = new Program();
+
+            // set line length
+            line.setLength(6.0);
+            Console.WriteLine("Length of line : {0}", line.getLength());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CustomizedException/CustomizedException/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CustomizedException
+{
+    public class InvalidAgeException : Exception
+    {
+        public InvalidAgeException(String message) : base(message)
+        {
+        }
+    }
+    class Program
+    {
+        static void validate(int age)
+        {
+            if (age < 18)
+            {
+                throw new InvalidAgeException("Sorry, Age is expected to be greater than 18");
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                validate(12);
+            }
+            catch (InvalidAgeException e)
+            {
+                Console.WriteLine(e);
+            }
+            Console.WriteLine("Catch block is being executed now.");
+        }
+    }
+}
