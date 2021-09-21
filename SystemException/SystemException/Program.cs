bec8209051fa0eb23d1fa1472293c9a6e4ac8a05
Diwakar Jaiswal <38224086+diwakarjaiswal880@users.noreply.github.com>
2021-09-21 12:00:08 +0530

    Add files via upload

---
+++ SystemException/SystemException/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SystemExceptionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] arr = new int[5];
+                arr[10] = 25;
+            }
+            catch (SystemException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
