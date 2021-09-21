1f638568854af77d0fd9a3fbc56b90a969e2007e
Diwakar Jaiswal <38224086+diwakarjaiswal880@users.noreply.github.com>
2021-09-21 11:56:48 +0530

    Add files via upload

---
+++ SortedSet/SortedSet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortedSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new SortedSet<string>();
+            names.Add("Sanjay");
+            names.Add("Anuradha");
+            names.Add("Praveen");
+            names.Add("Ravi");
+            names.Add("Praveen");
+            names.Add("Kajol");
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+    }
+}
---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> names = new Stack<string>();
+            names.Push("Chandan");
+            names.Push("Pooja");
+            names.Push("James");
+            names.Push("Rajesh");
+            names.Push("kumar");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+            Console.WriteLine("Peek element: " + names.Peek());
+            Console.WriteLine("Pop: " + names.Pop());
+            Console.WriteLine("After Pop, Peek element: " + names.Peek());
+        }
+    }
+}
