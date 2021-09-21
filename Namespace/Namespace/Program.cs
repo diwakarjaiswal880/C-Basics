465e882386e641de8ba42c58ab04a8867465ede7
Diwakar Jaiswal <38224086+diwakarjaiswal880@users.noreply.github.com>
2021-09-21 11:29:29 +0530

    Add files via upload

---
+++ Namespace/Namespace/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using First;
+using Second;
+namespace First
+{
+    public class Diwakar
+    {
+        public void sayWelcome() { Console.WriteLine("Welcome To Diwakar"); }
+    }
+}
+namespace Second
+{
+    public class Happy_Learning
+    {
+        public void sayWishes() { Console.WriteLine("Happy Learning"); }
+    }
+}
+namespace Namespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Diwakar h1 = new Diwakar();
+            Happy_Learning w1 = new Happy_Learning();
+            h1.sayWelcome();
+            w1.sayWishes();
+        }
+    }
+}
---
+++ OutParameter/OutParameter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OutParameter
+{
+    class Program
+    {
+        public void Show(out int val)
+        {
+            int square = 5;
+            val = square;
+            val *= val;
+        }
+        static void Main(string[] args)
+        {
+            int val = 50;
+            Program program = new Program();
+            Console.WriteLine("Value before passing out variable " + val);
+            program.Show(out val);
+            Console.WriteLine("Value after recieving the out variable " + val);
+        }
+    }
+}
