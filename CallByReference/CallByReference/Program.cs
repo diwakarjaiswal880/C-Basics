fa626385dd1479bde5dbd7afe80298b2906856d4
Diwakar Jaiswal <38224086+diwakarjaiswal880@users.noreply.github.com>
2021-09-21 10:03:47 +0530

    Add files via upload

---
+++ CallByReference/CallByReference/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CallByReference
+{
+    class Program
+    {
+        public void Show(ref int val)
+        {
+            val *= val;
+            Console.WriteLine("The value inside the show function " + val);
+        }
+        static void Main(string[] args)
+        {
+            int val = 50;
+            Program program = new Program();
+            Console.WriteLine("Value before calling the function " + val);
+            program.Show(ref val);
+            Console.WriteLine("Value after calling the function " + val);
+        }
+    }
+}
