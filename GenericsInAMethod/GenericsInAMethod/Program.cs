8bc83c27c4b3fcd4929b416d307548a6a33ad006
Diwakar Jaiswal <38224086+diwakarjaiswal880@users.noreply.github.com>
2021-09-21 11:11:15 +0530

    Add files via upload

---
+++ GenericsInAMethod/GenericsInAMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GenericsInAMethod
+{
+    class GenericClass
+    {
+        public void Show<T>(T msg)
+        {
+            Console.WriteLine(msg);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericClass genC = new GenericClass();
+            genC.Show("This message is from the generic method");
+            genC.Show(321);
+            genC.Show('H');
+        }
+    }
+}
---
+++ GetType/GetType/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GetType
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            char ch = 'd';
+            string  s1 = "diwakar";
+            String dj = "hello";
+            Type type1 = a.GetType();
+            Console.WriteLine(type1);
+            Type type2 = ch.GetType();
+            Console.WriteLine(type2);
+            Type type3 = s1.GetType();
+            Console.WriteLine(type3);
+            Type type4 = dj.GetType();
+            Console.WriteLine(type4);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(dj);
+
+            Type t = typeof(System.String);
+            Console.WriteLine(t.Assembly);
+
+            Console.WriteLine(t.FullName);
+            Console.WriteLine(t.BaseType);
+            Console.WriteLine(t.IsClass);
+            Console.WriteLine(t.IsEnum);
+            Console.WriteLine(t.IsInterface);
+        }
+    }
+}
