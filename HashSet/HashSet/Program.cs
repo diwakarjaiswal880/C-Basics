0502f295e56179dc59e665b9e1e755172ca4a66c
Diwakar Jaiswal <38224086+diwakarjaiswal880@users.noreply.github.com>
2021-09-21 11:13:48 +0530

    Add files via upload

---
+++ HashSet/HashSet/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new HashSet<string>();
+            names.Add("Sunil");
+            names.Add("Amar");
+            names.Add("Pujari");
+            names.Add("Imran");
+            names.Add("Amar");
+            names.Add("karan");
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Interface
+{
+    public interface Drawable
+    {
+        void draw();
+    }
+    public class Rectangle : Drawable
+    {
+        public void draw()
+        {
+            Console.WriteLine("drawing rectangle...");
+        }
+    }
+    public class Circle : Drawable
+    {
+        public void draw()
+        {
+            Console.WriteLine("drawing circle...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Drawable d;
+            d = new Rectangle();
+            d.draw();
+            d = new Circle();
+            d.draw();
+        }
+    }
+}
