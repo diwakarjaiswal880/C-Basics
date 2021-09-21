b2a3c0db026103c838e812260f33cc536ba0ccd1
Diwakar Jaiswal <38224086+diwakarjaiswal880@users.noreply.github.com>
2021-09-21 11:21:55 +0530

    Add files via upload

---
+++ List/List/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var names = new List<string>();
+            names.Add("Sandhya");
+            names.Add("Arun");
+            names.Add("Prashanth");
+            names.Add("Kiran");
+            foreach (var name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ MemberOverloading/MemberOverloading/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MemberOverloading
+{
+    public class Diwakar
+    {
+        public static int add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Diwakar.add(12, 23));
+            Console.WriteLine(Diwakar.add(12, 23, 25));
+        }
+    }
+}
